7dd55cfaad520a575f5fbd9deb9d1c4a4f2a2105
Egor Kraev <egor.kraev@gmail.com>
2018-11-16 16:23:32 +0000

    An apparently working cut of the cyk parser, supporting singular rules (nt -> nt) Just outputs decomposition cost for now, not the tree.

---
+++ src/generative_playground/codec/cyk.py
@@ -0,0 +1,202 @@
+from nltk.grammar import Nonterminal, Production, is_nonterminal
+import numpy as np
+import copy
+
+def binarize(p):
+    return _binarize(p,[])
+
+def get_unique_num():
+    n = 0
+    while True:
+        yield n
+        n+=1
+
+counter = get_unique_num()
+
+def _binarize(p, so_far=[]):
+    if len(p.rhs()) <= 2:
+        so_far.append(p)
+        return so_far
+    else:
+        new_nont = Nonterminal(p.lhs()._symbol + '_' +str(next(counter)))
+        so_far.append(Production(p.lhs(),[p.rhs()[0], new_nont]))
+        return _binarize(Production(new_nont, p.rhs()[1:]),so_far)
+
+def get_terminals_nonterminals(prods):
+    # preserve order of appearance for nonterminals so we know which token is root
+    terminals = set()
+    nonterminals = []
+    for p in prods:
+        if p.lhs() not in nonterminals:
+            nonterminals.append(p.lhs())
+        terminals = terminals.union(set(p.rhs()))
+
+    terminals = list(terminals.difference(set(nonterminals)))
+
+    return terminals, nonterminals
+
+def cyk_parser(prods, tokenized_seq):
+    # eliminate rules of the shape nonterminal -> nonterminal
+    # assume the lhs of first rule is root token
+    #non_sing_prods = eliminate_singular_rules(prods, prods[0].lhs())
+
+    # split all rules with len(rhs) > 2
+    new_prods = []
+    for p in prods:
+        new_prods += binarize(p)
+
+    # count and number all terminals
+    terminals, nonterminals = get_terminals_nonterminals(new_prods)
+
+    rule_by_lhs = {nt:[] for nt in nonterminals}
+    for p in new_prods:
+        rule_by_lhs[p.lhs()].append(p)
+
+    rule_cost = {rule: -1 for rule in new_prods + terminals}
+    # number all tokens
+    # this makes sure the root nonterminal gets index 0
+    ind = {val:i for i,val in enumerate(nonterminals + terminals)}
+
+    for t in tokenized_seq:
+        if t not in terminals:
+            raise ValueError('unknown token: ' + str(t))
+
+    # create a map from lhs to rhs indices
+    seq_len = len(tokenized_seq)
+
+    # run the cyk algorithm, modified to support rules with one item on the rhs
+    # meaning of the indices is span-1, start_ind, index of grammar rule
+    P = -1e6*np.ones([seq_len, seq_len, len(ind)])
+    for s, t in enumerate(tokenized_seq):
+        P[0, s, ind[t]] = rule_cost[t]
+
+    # go over singular rules at the same level until we know we got them all
+
+    def process_singular_rules(new_prods, P, L):
+        changed = True
+        while changed:
+            changed = False
+            for s, t in enumerate(tokenized_seq):
+                for prod in new_prods:
+                    if len(prod.rhs()) == 1:
+                        old_val = P[L, s, ind[prod.lhs()]]
+                        new_val = rule_cost[prod] + P[L, s, ind[prod.rhs()[0]]]
+                        if new_val > old_val:
+                            P[L, s, ind[prod.lhs()]] = new_val
+                            changed = True
+        return P
+
+    P = process_singular_rules(new_prods, P, 0)
+    print('****:',P[0,:,:].max())
+    # for each L = 1 to n-1 -- Length of span -1
+    for L in range(1,seq_len):
+        #   for each s = 0 to n-L -- Start of span-1
+        for s in range(seq_len-L):
+            # for each possible lhs
+            # process all 'normal' (len(rhs) ==1) rules
+            for lhs, prod_list in rule_by_lhs.items():
+                values = [np.zeros([0])]
+                # for all production rules with this lhs
+                for prod in prod_list:
+                    print(prod)
+                    # if the rule only has one item on the RHS:
+                    if len(prod.rhs()) == 2:
+                        tmp = np.zeros([L])
+                        for pp in range(L):
+                            tmp[pp] = rule_cost[prod] + P[pp, s, ind[prod.rhs()[0]]] \
+                                      + P[L - pp - 1, s + pp + 1, ind[prod.rhs()[1]]]
+                        values.append(tmp)
+                        print(tmp.max())
+                tmp2 = np.concatenate(values)
+                if len(tmp2) > 0:
+                    P[L,s,ind[lhs]] = tmp2.max()
+
+        # and now process all singular rules for this L
+        P = process_singular_rules(new_prods, P, L)
+        print('*********',L, P[L,:,:].max())
+    #           set back[l,s,a] = <p,b,c>
+
+    # convert back into a tree with the original grammar
+    return P[seq_len-1, 0, 0]
+
+def is_singular(prod):
+    return  len(prod.rhs()) == 1 and is_nonterminal(prod.rhs()[0])
+
+def eliminate_singular_rules(prods, root_token):
+    # eliminates all rules whose rhs has only one member, by substitution
+
+    terminals, nonterminals = get_terminals_nonterminals(prods)
+    # find all singular rules p with p.lhs = nt
+    # check if there are any non-singular rules or whether we can eliminate nt
+    singles = {nt: [] for nt in nonterminals}
+    others = {nt: [] for nt in nonterminals}
+    for prod in prods:
+        if is_singular(prod):
+            singles[prod.lhs()].append(prod)
+        elif len(prod.rhs()) >= 1:
+            others[prod.lhs()].append(prod)
+        else:
+            raise ValueError("rhs must have at least one member! "  + str(prod))
+
+    lhs_has_others = set(others)
+
+    new_prods = prods
+    # first replace all singular rules starting with root token:
+    new_prods = recursively_replace_root_singulars(new_prods, root_token)
+
+    # for each lhs with singulars:
+    for lhs, these_singles in singles.items():
+        new_prods = recursively_replace_lhs([p for p in new_prods if p not in these_singles],
+                                            lhs, these_singles, lhs in lhs_has_others)
+        if len(these_singles) > 0:
+            # after we replaced one lhs, need to index the remaining rules all over again
+            break
+
+    # check if we still have any singles left, and repeat until done
+    if any([len(prod.rhs()) == 1 and is_nonterminal(prod.rhs()[0]) for prod in prods]):
+        return eliminate_singular_rules(new_prods, root_token)
+    else:
+        return new_prods
+
+def recursively_replace_root_singulars(rules, root_token):
+    # find a singular root rule
+    found = False
+    for root_rule in rules:
+        if root_rule.lhs() == root_token and is_singular(root_rule):
+            found = True
+            break
+
+    if not found: # no more root singulars
+        return rules
+
+    other_rules = [p for p in rules if p != root_rule]
+    new_rules = copy.copy(other_rules)
+    for othr in other_rules:
+        if othr.lhs() == root_rule.rhs()[0]:
+            new_rules = [Production(root_token, othr.rhs())] + new_rules
+
+    return recursively_replace_root_singulars(new_rules, root_token)
+
+
+
+def recursively_replace_lhs(rules, lhs, singles, keep_original):
+    assert(all([lhs == p.lhs() for p in singles]))
+    assert(all([len(p.rhs()) == 1 for p in singles]))
+    out = []
+    for r in rules:
+        if lhs not in r.rhs():
+            out += [r]
+        else:
+            if keep_original:
+                out += [r]
+            # find first occurrence
+            for loc, t in enumerate(r.rhs()):
+                if t==lhs:
+                    break
+            # substitute first occurrence
+            new_rules = [Production(r.lhs(),
+                                    list(r.rhs()[:loc]) + list(s.rhs()) + list(r.rhs()[loc+1:]))
+                         for s in singles]
+            out += recursively_replace_lhs(new_rules, lhs, singles, keep_original)
+
+    return out
---
+++ src/generative_playground/codec/rpe.py
@@ -0,0 +1,93 @@
+from nltk.tree import *
+import nltk
+from nltk.grammar import Nonterminal, Production, is_nonterminal
+import numpy as np
+from generative_playground.codec.cyk import *
+
+from generative_playground.codec.grammar_codec import GrammarModel
+from generative_playground.models.model_settings import get_decoder, get_settings
+
+settings = get_settings(True, 'new')
+from generative_playground.codec.smiles_grammar_new_2 import grammar_string_zinc_new
+from generative_playground.codec.grammar_helper import GrammarHelper
+grammar_string = grammar_string_zinc_new
+
+
+fname = '../data/250k_rndm_zinc_drugs_clean.smi'
+
+with open(fname) as f:
+    smiles = f.readlines()
+
+for i in range(len(smiles)):
+    smiles[i] = smiles[i].strip()
+
+def parse(x, codec):
+    # print(x)
+    try:
+        return next(codec._parser.parse(x))
+    except Exception as e:
+        #         print('fail!')
+        #         print(e)
+        return None
+
+for _ in range(10):
+    codec = GrammarModel(grammar=GrammarHelper(grammar_string)
+    , tokenizer=settings['tokenizer'])
+
+
+
+    tokens = [codec._tokenize(s) for s in smiles[:10]]
+
+    parse_trees = [(t,parse(t, codec)) for t in tokens]
+    tokens = [p[0] for p in parse_trees if p[1] is not None]
+    parse_trees = [p[1] for p in parse_trees if p[1] is not None]
+
+
+    GCFG = nltk.CFG.fromstring(grammar_string)
+    prods = GCFG.productions()
+    # assume the lhs of first rule is root token
+    no_sing = eliminate_singular_rules(prods, prods[0].lhs())
+    parseable = cyk_parser(prods, ['F','F'])
+
+    print("average productions:", np.array([len(p.productions()) for p in parse_trees]).mean())
+
+    rpe_dict ={}
+
+    def get_rpe(tree, rpe_dict):
+        these_tuples = [(tree.productions()[0], loc, child.productions()[0]) for loc,child in enumerate(tree) if isinstance(child,Tree)]
+        for t in these_tuples:
+            if t in rpe_dict:
+                rpe_dict[t] += 1
+            else: rpe_dict[t] = 1
+        for subtree in tree:
+            if isinstance(subtree, Tree):
+                rpe_dict = get_rpe(subtree, rpe_dict)
+        return rpe_dict
+    for tree in parse_trees:
+        rpe_dict = get_rpe(tree,rpe_dict)
+    #print(rpe_dict)
+    rpe_count = [(key, value) for key, value in rpe_dict.items()]
+    rpe_count = sorted(rpe_count, key = lambda x:x[1],reverse = True)
+    print(len(parse_trees))
+    for x in rpe_count[:1]:
+        print(x)
+
+    first_rule, loc, second_rule = rpe_count[0][0]
+    new_rhs = []
+    count = 0
+    for t in first_rule.rhs():
+        if is_nonterminal(t) and count == loc:
+            new_rhs += second_rule.rhs()
+        else:
+            new_rhs.append(t)
+
+    print(first_rule)
+    print(second_rule)
+    print(new_rhs)
+    new_prod = str(Production(first_rule.lhs(), new_rhs))
+    print(new_prod)
+    grammar_string = grammar_string.split('\n')
+    new_prods = grammar_string[:1] + [new_prod] + grammar_string[1:]
+    grammar_string ="\n".join([str(p).replace('\\\\','\\')  for p in new_prods])
+
+print('done!')
