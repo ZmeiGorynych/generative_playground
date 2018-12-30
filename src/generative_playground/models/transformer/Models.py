bb816c42832b4cb5e25e1b3c831835bd6c9a509e
Egor Kraev <egor.kraev@gmail.com>
2018-12-30 11:29:04 +0000

    Factor out embedding logic from the Encoder

--- src/generative_playground/models/transformer/Models.py
+++ src/generative_playground/models/transformer/Models.py
@@ -43,6 +43,66 @@ def get_attn_subsequent_mask(seq):
         subsequent_mask = subsequent_mask.cuda()
     return subsequent_mask
 
+
+class InputSequenceNormalizer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, src_seq):
+        '''
+        Converts sequence from 3d one-hot to 2d longs, if necessary; and generates the src_seq_for_masking for the transformer
+        :param src_seq: batch x num_steps x feature_dim long, or batch x num_steps long or float
+        :return:
+        '''
+        if src_seq.dtype == torch.int64:  # indices of discrete actions
+            if len(src_seq.size()) == 3:  # if got a one-hot encoded vector
+                src_seq = torch.max(src_seq, 2)[-1]  # argmax
+            src_seq_for_masking = src_seq
+        elif src_seq.dtype == torch.float32 and len(src_seq.size()) == 3:  # if the input is continuous
+            src_seq_for_masking = torch.ones(src_seq.size()[:2], device=src_seq.device)
+
+        return src_seq, src_seq_for_masking
+
+class Embedder(nn.Module):
+    def __init__(self,
+                 n_max_seq,
+                 n_src_vocab,  # feature_len
+                 d_model=512,  # 128,#
+                 padding_idx=Constants.PAD,  # TODO: remember to set this to n_src_vocab-1 when calling from my code!
+                 encode_position = True
+                 ):
+        super().__init__()
+        n_position = n_max_seq + 1
+        self.encode_position = encode_position
+        self.position_enc = nn.Embedding(n_position, d_model, padding_idx=padding_idx)
+        self.position_enc.weight.data = position_encoding_init(n_position, d_model)
+        self.src_word_emb = nn.Embedding(n_src_vocab, d_model, padding_idx=padding_idx)
+        self.src_vector_fc = nn.Linear(n_src_vocab, d_model)
+
+    def forward(self, src_seq, src_pos=None):
+        '''
+        Embed the source sequence, with optional position specification
+        :param src_seq: batch x num_steps long or batch x num_steps x feature_size
+        :param src_pos: batch x num_steps long, or None
+        :return:
+        '''
+
+        if src_seq.dtype == torch.int64:  # indices of discrete actions
+            enc_input = self.src_word_emb(src_seq)
+        elif src_seq.dtype == torch.float32 and len(src_seq.size()) == 3:  # if the input is continuous
+            enc_input = self.src_vector_fc(src_seq)
+
+        # Position Encoding addition
+        if self.encode_position:
+            if src_pos is None:
+                batch_size = src_seq.size()[0]
+                seq_len = src_seq.size()[1]
+                src_pos = to_gpu(torch.arange(seq_len).unsqueeze(0).expand(batch_size,seq_len).type(LongTensor))
+
+            enc_input += self.position_enc(src_pos)
+        return enc_input
+
+
 class TransformerEncoder(nn.Module):
     ''' A encoder model with self attention mechanism. '''
 
@@ -53,7 +113,6 @@ class TransformerEncoder(nn.Module):
                  n_head=8,  #6,
                  d_k=64,  #16,
                  d_v=64,  #16,#
-                 d_word_vec=512,  #128,#
                  d_model=512,  #128,#
                  d_inner_hid=1024,  #256,#
                  dropout=0.1,
@@ -63,18 +122,16 @@ class TransformerEncoder(nn.Module):
                  ):
 
         super(TransformerEncoder, self).__init__()
-
-        n_position = n_max_seq + 1
+        self.embedder = Embedder(n_max_seq,
+                                 n_src_vocab,
+                                 d_model,
+                                 padding_idx)
         self.n_max_seq = n_max_seq
         self.d_model = d_model
         self.n_head = n_head
         self.include_self_attention = use_self_attention
         self.transpose_self_attention = transpose_self_attention
-        self.position_enc = nn.Embedding(n_position, d_word_vec, padding_idx=padding_idx)
-        self.position_enc.weight.data = position_encoding_init(n_position, d_word_vec)
-
-        self.src_word_emb = nn.Embedding(n_src_vocab, d_word_vec, padding_idx=padding_idx)
-        self.src_vector_fc = nn.Linear(n_src_vocab, d_word_vec)
+        self.normalizer = InputSequenceNormalizer()
         self.layer_stack = nn.ModuleList([
             EncoderLayer(d_model,
                          d_inner_hid,
@@ -90,9 +147,6 @@ class TransformerEncoder(nn.Module):
 
         self.output_shape = [None, n_max_seq, d_model]
 
-        # if z_size is not None:
-        #     self.z_enc = nn.Linear(d_model, z_size)
-
     def forward(self, src_seq, src_pos=None):
         '''
 
@@ -101,26 +155,9 @@ class TransformerEncoder(nn.Module):
         :param return_attns:
         :return: batch_size x n_max_seq x d_model
         '''
-        batch_size = src_seq.size()[0]
-        seq_len = src_seq.size()[1]
 
-        # normalize the input vector:
-        if src_seq.dtype == torch.int64: # indices of discrete actions
-            if len(src_seq.size()) == 3: # if got a one-hot encoded vector
-                src_seq = torch.max(src_seq,2)[-1] # argmax
-
-            # Word embedding look up
-            enc_input = self.src_word_emb(src_seq)
-            src_seq_for_masking = src_seq
-        elif src_seq.dtype == torch.float32 and len(src_seq.size()) == 3: # if the input is continuous
-            enc_input = self.src_vector_fc(src_seq)
-            src_seq_for_masking = torch.ones(src_seq.size()[:2], device=src_seq.device)
-
-        # Position Encoding addition
-        if src_pos is None:
-            src_pos = to_gpu(torch.arange(seq_len).unsqueeze(0).expand(batch_size,seq_len).type(LongTensor))
-
-        enc_input += self.position_enc(src_pos)
+        src_seq, src_seq_for_masking = self.normalizer(src_seq)
+        enc_input = self.embedder(src_seq, src_pos)
         if self.include_self_attention:
             enc_slf_attns = []
 
@@ -213,59 +250,59 @@ class TransformerDecoder(nn.Module):
             return dec_output,
 
 
-class Transformer(nn.Module):
-    ''' A sequence to sequence model with attention mechanism. '''
-
-    def __init__(
-            self, n_src_vocab, n_tgt_vocab, n_max_seq, n_layers=6, n_head=8,
-            d_word_vec=512, d_model=512, d_inner_hid=1024, d_k=64, d_v=64,
-            dropout=0.1, proj_share_weight=True, embs_share_weight=True):
-
-        super(Transformer, self).__init__()
-        self.encoder = TransformerEncoder(
-            n_src_vocab, n_max_seq, n_layers=n_layers, n_head=n_head,
-            d_word_vec=d_word_vec, d_model=d_model,
-            d_inner_hid=d_inner_hid, dropout=dropout)
-        self.decoder = TransformerDecoder(
-            n_tgt_vocab, n_max_seq, n_layers=n_layers, n_head=n_head,
-            d_word_vec=d_word_vec, d_model=d_model,
-            d_inner_hid=d_inner_hid, dropout=dropout)
-        self.tgt_word_proj = Linear(d_model, n_tgt_vocab, bias=False)
-        self.dropout = nn.Dropout(dropout)
-
-        assert d_model == d_word_vec, \
-        'To facilitate the residual connections, \
-         the dimensions of all module output shall be the same.'
-
-        if proj_share_weight:
-            # Share the weight matrix between tgt word embedding/projection
-            assert d_model == d_word_vec
-            self.tgt_word_proj.weight = self.decoder.tgt_word_emb.weight
-
-        if embs_share_weight:
-            # Share the weight matrix between src/tgt word embeddings
-            # assume the src/tgt word vec size are the same
-            assert n_src_vocab == n_tgt_vocab, \
-            "To share word embedding table, the vocabulary size of src/tgt shall be the same."
-            self.encoder.src_word_emb.weight = self.decoder.tgt_word_emb.weight
-
-    #TODO: do we need this? Or just set requires_grad to False, or not even register them as params!
-    def get_trainable_parameters(self):
-        ''' Avoid updating the position encoding '''
-        enc_freezed_param_ids = set(map(id, self.encoder.position_enc.parameters()))
-        dec_freezed_param_ids = set(map(id, self.decoder.position_enc.parameters()))
-        freezed_param_ids = enc_freezed_param_ids | dec_freezed_param_ids
-        return (p for p in self.parameters() if id(p) not in freezed_param_ids)
-
-    def forward(self, src, tgt):
-        src_seq, src_pos = src
-        tgt_seq, tgt_pos = tgt
-
-        tgt_seq = tgt_seq[:, :-1]
-        tgt_pos = tgt_pos[:, :-1]
-
-        enc_output, *_ = self.encoder(src_seq, src_pos)
-        dec_output, *_ = self.decoder(tgt_seq, tgt_pos, src_seq, enc_output)
-        seq_logit = self.tgt_word_proj(dec_output)
-
-        return seq_logit.view(-1, seq_logit.size(2))
+# class Transformer(nn.Module):
+#     ''' A sequence to sequence model with attention mechanism. '''
+#
+#     def __init__(
+#             self, n_src_vocab, n_tgt_vocab, n_max_seq, n_layers=6, n_head=8,
+#             d_word_vec=512, d_model=512, d_inner_hid=1024, d_k=64, d_v=64,
+#             dropout=0.1, proj_share_weight=True, embs_share_weight=True):
+#
+#         super(Transformer, self).__init__()
+#         self.encoder = TransformerEncoder(
+#             n_src_vocab, n_max_seq, n_layers=n_layers, n_head=n_head,
+#             d_word_vec=d_word_vec, d_model=d_model,
+#             d_inner_hid=d_inner_hid, dropout=dropout)
+#         self.decoder = TransformerDecoder(
+#             n_tgt_vocab, n_max_seq, n_layers=n_layers, n_head=n_head,
+#             d_word_vec=d_word_vec, d_model=d_model,
+#             d_inner_hid=d_inner_hid, dropout=dropout)
+#         self.tgt_word_proj = Linear(d_model, n_tgt_vocab, bias=False)
+#         self.dropout = nn.Dropout(dropout)
+#
+#         assert d_model == d_word_vec, \
+#         'To facilitate the residual connections, \
+#          the dimensions of all module output shall be the same.'
+#
+#         if proj_share_weight:
+#             # Share the weight matrix between tgt word embedding/projection
+#             assert d_model == d_word_vec
+#             self.tgt_word_proj.weight = self.decoder.tgt_word_emb.weight
+#
+#         if embs_share_weight:
+#             # Share the weight matrix between src/tgt word embeddings
+#             # assume the src/tgt word vec size are the same
+#             assert n_src_vocab == n_tgt_vocab, \
+#             "To share word embedding table, the vocabulary size of src/tgt shall be the same."
+#             self.encoder.src_word_emb.weight = self.decoder.tgt_word_emb.weight
+#
+#     #TODO: just set requires_grad to False instead
+#     def get_trainable_parameters(self):
+#         ''' Avoid updating the position encoding '''
+#         enc_freezed_param_ids = set(map(id, self.encoder.position_enc.parameters()))
+#         dec_freezed_param_ids = set(map(id, self.decoder.position_enc.parameters()))
+#         freezed_param_ids = enc_freezed_param_ids | dec_freezed_param_ids
+#         return (p for p in self.parameters() if id(p) not in freezed_param_ids)
+#
+#     def forward(self, src, tgt):
+#         src_seq, src_pos = src
+#         tgt_seq, tgt_pos = tgt
+#
+#         tgt_seq = tgt_seq[:, :-1]
+#         tgt_pos = tgt_pos[:, :-1]
+#
+#         enc_output, *_ = self.encoder(src_seq, src_pos)
+#         dec_output, *_ = self.decoder(tgt_seq, tgt_pos, src_seq, enc_output)
+#         seq_logit = self.tgt_word_proj(dec_output)
+#
+#         return seq_logit.view(-1, seq_logit.size(2))
