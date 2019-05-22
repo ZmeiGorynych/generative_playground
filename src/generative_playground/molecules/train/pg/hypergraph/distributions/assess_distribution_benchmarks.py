3f21cdf2130b81a307d5bff136d7f061aa78bdc6
Egor Kraev <egor.kraev@gmail.com>
2019-05-22 21:51:46 +0100

    Commit the file names to use for benchmarks

--- src/generative_playground/molecules/train/pg/hypergraph/distributions/assess_distribution_benchmarks.py
+++ src/generative_playground/molecules/train/pg/hypergraph/distributions/assess_distribution_benchmarks.py
@@ -2,15 +2,21 @@ from guacamol.assess_distribution_learning import assess_distribution_learning
 from generative_playground.molecules.guacamol_utils import DummyMoleculeGenerator
 from generative_playground.molecules.lean_settings import get_data_location
 
-
 # # naive sampling
 # train_mols = get_data_location(source="ChEMBL:train")['source_data']
 # my_gen = DummyMoleculeGenerator('distribution_naive_smiles.zip', maximize_reward=False)
 # assess_distribution_learning(my_gen, train_mols)
 
 # stable-ish run with discriminator sampling
+#
+files = [
+    'distribution_naive_no_priors_smiles.zip',
+    'distribution_naive_uncond_priors_smiles.zip',  # is the one with unconditional priors
+    'distribution_naive_smiles.zip',  # is the one with conditional priors
+    'distribution_discr_eps0.2_smiles.zip']
+
 train_mols = get_data_location(source="ChEMBL:train")['source_data']
-my_gen = DummyMoleculeGenerator('distribution_discr_copy.zip',#eps0.2_smiles.zip',
+my_gen = DummyMoleculeGenerator(files[0],  # eps0.2_smiles.zip',
                                 maximize_reward=False,
                                 keep_last=1e4)
 assess_distribution_learning(my_gen, train_mols)
