4cd098753cc29b2613b8ef84b1d49e0897f47adb
Mark Harley <mark@mosaicsmartdata.com>
2019-08-16 18:39:07 +0100

    Working batch_run script

--- src/generative_playground/utils/batch_run.py
+++ src/generative_playground/utils/batch_run.py
@@ -1,27 +1,37 @@
+from typing import Dict, List
+
 from fabric import Connection
 
 
-SOURCE_ROOT = '/home/ubuntu/generative_playground/generative_playground/src'
-PYTHON_FILE = '{}/models/problem/mcts/mcts.py'.format(SOURCE_ROOT)
-KEY_FILE = '~/keys/mark.pem'
-job_assignments = {
-    'ip': []
-}
-
-
-for ip, jobs in job_assignments.items():
-    c = Connection(
-        'ubuntu@{}'.format(ip),
-        connect_kwargs={'key_filename': KEY_FILE}
-    )
-    for job in jobs:
-        c.run(
-            (
-                'screen -d -m '
-                'source activate pytorch_p36 && '
-                'export PYTHONPATH=$PYTHONPATH:{src_root}/ && '
-                'python {python_file} {job_id}'
-            ).format(src_root=SOURCE_ROOT, python_file=PYTHON_FILE, job_id=job)
+def batch_run(
+    source_root: str,
+    python_file: str,
+    key_file: str,
+    job_assignments: Dict[str, List[str]]
+) -> None:
+    for ip, jobs in job_assignments.items():
+        c = Connection(
+            'ubuntu@{}'.format(ip),
+            connect_kwargs={'key_filename': key_file}
         )
-        print('Started job {}'.format(job))
-print('All jobs running')
+        for job in jobs:
+            c.run(
+                (
+                    "screen -dmS mols; "
+                    "screen -S mols -X stuff 'source activate pytorch_p36'$(echo -ne '\015'); "
+                    "screen -S mols -X stuff 'export PYTHONPATH=$PYTHONPATH:{src_root}/'$(echo -ne '\015'); "
+                    "screen -S mols -X stuff 'python {python_file} {job_id}'$(echo -ne '\015');"
+                ).format(src_root=source_root, python_file=python_file, job_id=job)
+            )
+            print('Started job {}'.format(job))
+    print('All jobs running')
+
+
+if __name__ == '__main__':
+    SOURCE_ROOT = '/home/ubuntu/shared/GitHub/generative_playground/src'
+    PYTHON_FILE = '{}/generative_playground/molecules/train/pg/conditional/v2/train_conditional_v2_0.py'.format(SOURCE_ROOT)
+    KEY_FILE = '/home/mark/keys/aws_second_key_pair.pem'
+    job_assignments = {
+        '34.242.186.107': ["1 --attempt test_fabric"],
+    }
+    batch_run(SOURCE_ROOT, PYTHON_FILE, KEY_FILE, job_assignments)
